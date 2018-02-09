8f16da3d703b5588eb3b1e925937e5f17c9aad92
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-02-09 10:44:38 -0800

    Adjust params type

--- exp_to_json.py
+++ exp_to_json.py
@@ -1,85 +1,76 @@
-import argparse
-import json
 import re
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file")
-args = parser.parse_args()
 
-exp_dict = {
-    'so_job_table': {},
-    'so_job_prompts': [],
-    'so_object_cond': []
-}
-check_dict = {
-    'B': [],  # program types
-    'D': [],  # data type
-    'G': [],  # agents
-    'K': [],  # application
-    'L': [],  # pathes
-    'O': [],  # logins
-    'Q': [],  # queues
-    'n': [],  # notifications
-    'R': []   # roles
-}
-is_job_table = is_job_prompts = is_object_cond = False
+def exp_to_json(file_path):
 
-with open(args.file, 'r') as f:
+    exp_json = {
+        'so_job_table': {},
+        'so_job_prompts': [],
+        'so_object_cond': []
+    }
+    check_dict = {
+        'B': [],  # program types
+        'D': [],  # data type
+        'G': [],  # agents
+        'K': [],  # application
+        'L': [],  # pathes
+        'O': [],  # logins
+        'Q': [],  # queues
+        'n': [],  # notifications
+        'R': []   # roles
+    }
+    is_job_table = is_job_prompts = is_object_cond = False
 
-    for line in f:
-        if line.startswith('CHECK'):
-            match = re.search(r'^CHECK (.*) (.*)', line)
-            check_dict[match.group(1)].append(match.group(2))
+    with open(file_path, 'r') as f:
 
-        # so_job_table
-        if line.startswith('START=so_job_table'):
-            is_job_table = True
-            so_module = re.search(r'so_module=([^\s]*)', line).group(1)
-            exp_dict['so_job_table'] = {'so_module': so_module, 'params': []}
+        for line in f:
+            if line.startswith('CHECK'):
+                match = re.search(r'^CHECK (.*) (.*)', line)
+                check_dict[match.group(1)].append(match.group(2))
 
-        # so_job_prompts
-        if line.startswith('START=so_job_prompts'):
-            is_job_prompts = True
-            so_module = re.search(r'so_module=([^\s]*)', line).group(1)
-            so_prompt = re.search(r'so_prompt=([^\s]*)', line).group(1)
-            exp_dict['so_job_prompts'].append({
-                'so_module': so_module,
-                'so_prompt': so_prompt,
-                'params': []
-            })
+            # so_job_table
+            if line.startswith('START=so_job_table'):
+                is_job_table = True
+                so_module = re.search(r'so_module=([^\s]*)', line).group(1)
+                exp_json['so_job_table'] = {'so_module': so_module, 'params': {}}
 
-        # so_object_cond
-        if line.startswith('START=so_object_cond'):
-            is_object_cond = True
-            so_module = re.search(r'so_module=([^\s]*)', line).group(1)
-            so_soc_order = re.search(r'so_soc_order=([^\s]*)', line).group(1)
-            so_obj_type = re.search(r'so_obj_type=([^\s]*)', line).group(1)
-            exp_dict['so_object_cond'].append({
-                'so_module': so_module,
-                'so_soc_order': so_soc_order,
-                'so_obj_type': so_obj_type,
-                'params': []
-            })
+            # so_job_prompts
+            if line.startswith('START=so_job_prompts'):
+                is_job_prompts = True
+                so_module = re.search(r'so_module=([^\s]*)', line).group(1)
+                so_prompt = re.search(r'so_prompt=([^\s]*)', line).group(1)
+                exp_json['so_job_prompts'].append({
+                    'so_module': so_module,
+                    'so_prompt': so_prompt,
+                    'params': {}
+                })
 
-        # END reset
-        if line.startswith('END'):
-            is_job_table = is_job_prompts = is_object_cond = False
+            # so_object_cond
+            if line.startswith('START=so_object_cond'):
+                is_object_cond = True
+                so_module = re.search(r'so_module=([^\s]*)', line).group(1)
+                so_soc_order = re.search(r'so_soc_order=([^\s]*)', line).group(1)
+                so_obj_type = re.search(r'so_obj_type=([^\s]*)', line).group(1)
+                exp_json['so_object_cond'].append({
+                    'so_module': so_module,
+                    'so_soc_order': so_soc_order,
+                    'so_obj_type': so_obj_type,
+                    'params': {}
+                })
 
-        match = re.search(r'^(.*?)=(.*)', line)
-        if match and is_job_table:
-            exp_dict['so_job_table']['params'].append({
-                match.group(1): match.group(2)
-            })
-        elif match and is_job_prompts:
-            exp_dict['so_job_prompts'][int(so_prompt)-1]['params'].append({
-                match.group(1): match.group(2)
-            })
-        elif match and is_object_cond:
-            exp_dict['so_object_cond'][int(so_soc_order)-1]['params'].append({
-                match.group(1): match.group(2)
-            })
+            # END reset
+            if line.startswith('END'):
+                is_job_table = is_job_prompts = is_object_cond = False
 
-        # TODO: need to handle line starts with 'DELETE'?
+            match = re.search(r'^(.*?)=(.*)', line)
+            if match and is_job_table:
+                exp_json['so_job_table']['params'][match.group(1)] = match.group(2)
+            elif match and is_job_prompts:
+                exp_json['so_job_prompts'][int(so_prompt)-1]['params'][match.group(1)] = match.group(2)
+            elif match and is_object_cond:
+                exp_json['so_object_cond'][int(so_soc_order)-1]['params'][match.group(1)] = match.group(2)
 
-exp_dict['checks'] = check_dict
-print(json.dumps(exp_dict, indent=4))
+            # TODO: need to handle line starts with 'DELETE'?
+
+    exp_json['checks'] = check_dict
+    return exp_json
